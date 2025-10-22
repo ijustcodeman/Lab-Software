a3baa94553418e789f778d9003841aafca36282f
Max Gebert <maxgebert1710@gmail.com>
2025-10-22 11:57:38 +0200

    Finished

---
+++ Labor3/Aufgabe3/src/Familie.java
@@ -0,0 +1,134 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+
+/**
+ * Diese Klasse repräsentiert eine Familie, welche aus Vater, Mutter und Kindern bestehen kann.
+ * @author Max Gebert, 21513
+ */
+public class Familie implements Iterable<String>{
+
+    /**
+     * Liste zum speichern aller Familienmitglieder
+     */
+    private ArrayList<String> mitglieder;
+
+    /**
+     * Wahrheitswert zum Prüfen, ob ein Kind einen Vater hat
+     */
+    private boolean hatVater;
+
+    /**
+     * Wahrheitswert zum Prüfen, ob ein Kind eine Mutter hat.
+     */
+    private boolean hatMutter;
+
+    /**
+     * Konstruktor zum erstellen einer Familie mit nur einen Elternteil.
+     * @param _elternTeil Name vom Elternteil
+     * @param istVater true für Vater, false für Mutter
+     */
+    public Familie(String _elternTeil, boolean istVater){
+        if (_elternTeil == null || _elternTeil.isEmpty()){
+            throw new IllegalArgumentException("Der Name darf nicht leer sein.");
+        }
+        mitglieder = new ArrayList<>();
+        mitglieder.add(_elternTeil);
+
+        this.hatVater = istVater;
+        this.hatMutter = !istVater;
+    }
+
+    /**
+     * Konstruktor zum erstellen einer Familie mit Vater und Mutter.
+     * @param _vater Name vom Vater
+     * @param _mutter Name der Mutter
+     */
+    public Familie(String _vater, String _mutter){
+        if (_vater == null || _mutter == null || _vater.isEmpty() || _mutter.isEmpty()){
+            throw new IllegalArgumentException("Der Name darf nicht leer sein.");
+        }
+        mitglieder = new ArrayList<>();
+        mitglieder.add(_vater);
+        mitglieder.add(_mutter);
+
+        this.hatVater = true;
+        this.hatMutter = true;
+    }
+
+    /**
+     * Methode, um ein Knd zur Familie hinzuzufügen.
+     * @param _kind Name vom Kind
+     */
+    public void addKind(String _kind){
+        if (_kind == null){
+            throw new IllegalArgumentException("Der Name darf nicht Null sein.");
+        }
+        this.mitglieder.add(_kind);
+    }
+
+    /**
+     * Enum mit Vater, Mutter und Kind.
+     */
+    public enum Familienmitglied{
+        VATER,
+        MUTTER,
+        KINDER;
+    }
+
+    /**
+     * Gibt ein Familienmitglied der Familie als String zurück.
+     * @param mitglied Das Familienmitglid
+     * @return Das Familienmitglied als String
+     */
+    public String getMitglied(Familienmitglied mitglied){
+
+        switch (mitglied){
+            case VATER:
+                if (hatVater){
+                    return mitglieder.get(0);
+                }
+                else{
+                    return "";
+                }
+
+            case MUTTER:
+                if (hatMutter){
+                    if (hatVater){
+                        return mitglieder.get(1);
+                    }
+                    else{
+                        return mitglieder.get(0);
+                    }
+                }
+                else{
+                    return "";
+                }
+
+            case KINDER:
+                int startIndex = hatMutter && hatVater ? 2 : 1;
+                if (mitglieder.size() <= startIndex){
+                    return "";
+                }
+                StringBuilder kinder = new StringBuilder();
+                for (int i = startIndex; i < mitglieder.size(); i++) {
+                    kinder.append(mitglieder.get(i));
+                    if (i < mitglieder.size() - 1) {
+                        kinder.append(", ");
+                    }
+                }
+                return kinder.toString();
+
+            default:
+                return "";
+        }
+    }
+
+    /**
+     * Iterator für die Liste.
+     * @return Alle Namen der Familie
+     */
+    @Override
+    public Iterator<String> iterator() {
+        return mitglieder.iterator();
+    }
+}
---
+++ Labor3/Aufgabe3/src/Program.java
@@ -0,0 +1,63 @@
+import java.util.Iterator;
+
+/**
+ * Hauptklasse zum Testen verschiedenster Testfälle.
+ * @author Max Gebert, 21513
+ */
+public class Program {
+
+    /**
+     * Hauptmethode für das Instanziieren und ausgeben auf der Konsole.
+     * @param args Wird hier nicht genutzt
+     */
+    public static void main(String[] args){
+
+        Familie schmidt = new Familie("Robert Schmidt", "Andrea Schmidt");
+        schmidt.addKind("Noah Schmidt");
+        schmidt.addKind("Alex Schmidt");
+
+        Familie mueller = new Familie("Rene Mueller", "Sandra Mueller");
+
+        // Familie mit Kinder
+        for (Familie.Familienmitglied mitglied: Familie.Familienmitglied.values()){
+            System.out.println(schmidt.getMitglied(mitglied));
+        }
+        System.out.println();
+
+        // Familie ohne Kinder
+        for (Familie.Familienmitglied mitglied: Familie.Familienmitglied.values()){
+            System.out.println(mueller.getMitglied(mitglied));
+        }
+
+        // Alleinerziehender Vater mit Kinder
+        Familie mustermann = new Familie("Max Mustermann", true);
+        mustermann.addKind("Nico Mustermann");
+        mustermann.addKind("Lisa Mustermann");
+
+        for (Familie.Familienmitglied mitglied: Familie.Familienmitglied.values()){
+            System.out.println(mustermann.getMitglied(mitglied));
+        }
+
+        // Eine Frau
+        Familie schulz = new Familie("Julie Schulz", false);
+        for (Familie.Familienmitglied mitglied: Familie.Familienmitglied.values()){
+            System.out.println(schulz.getMitglied(mitglied));
+        }
+
+        // Nutzung vom Iterator
+        for (String name : schmidt) {
+            System.out.println(name);
+        }
+
+        // Beispiel, warum Datenkapselung gefährdet ist
+        Iterator<String> it = schmidt.iterator();
+        it.next();
+        it.remove();
+
+        System.out.println();
+
+        for (String name : schmidt) {
+            System.out.println(name);
+        }
+    }
+}
