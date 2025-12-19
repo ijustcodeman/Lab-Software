96fbca406407ad1064a39efb8480629b95ace24e
Max Gebert <maxgebert1710@gmail.com>
2025-12-19 14:59:07 +0100

    Changed to the required GUI

--- Labor5/WikiBooks/src/app/WikiBooksApplication.java
+++ Labor5/WikiBooks/src/app/WikiBooksApplication.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.scene.image.Image;
 
 import java.io.IOException;
 
@@ -13,9 +14,20 @@ public class WikiBooksApplication extends Application {
     public void start(Stage stage) throws IOException {
 
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("WikiBooksUI.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 800, 600);
 
-        stage.setTitle("WikiBooks");
+        Scene scene = new Scene(fxmlLoader.load(), 900, 650);
+
+        stage.setTitle("Mein WikibooksBrowser");
+
+        try {
+            stage.getIcons().add(new Image(getClass().getResourceAsStream("/images/img.png")));
+        } catch (Exception e) {
+            System.out.println("Icon konnte nicht geladen werden: " + e.getMessage());
+        }
+
+        stage.setMinWidth(900);
+        stage.setMinHeight(650);
+
         stage.setScene(scene);
         stage.show();
 
